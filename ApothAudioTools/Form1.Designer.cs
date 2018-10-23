2eded3f908d3d2f8d098e6fd0375c6867f281131
David Waidmann <David.Waidmann@flyfrontier.com>
2018-10-23 14:35:45 -0600

    initial commit

---
+++ ApothAudioTools/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ApothAudioTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.lblResolution = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxUrl = new System.Windows.Forms.TextBox();
+            this.cmbxResolution = new System.Windows.Forms.ComboBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(58, 37);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(23, 13);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "Url:";
+            // 
+            // lblResolution
+            // 
+            this.lblResolution.AutoSize = true;
+            this.lblResolution.Location = new System.Drawing.Point(21, 62);
+            this.lblResolution.Name = "lblResolution";
+            this.lblResolution.Size = new System.Drawing.Size(60, 13);
+            this.lblResolution.TabIndex = 1;
+            this.lblResolution.Text = "Resolution:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Progress:";
+            // 
+            // tbxUrl
+            // 
+            this.tbxUrl.Location = new System.Drawing.Point(87, 30);
+            this.tbxUrl.Name = "tbxUrl";
+            this.tbxUrl.Size = new System.Drawing.Size(660, 20);
+            this.tbxUrl.TabIndex = 3;
+            // 
+            // cmbxResolution
+            // 
+            this.cmbxResolution.FormattingEnabled = true;
+            this.cmbxResolution.Location = new System.Drawing.Point(87, 56);
+            this.cmbxResolution.Name = "cmbxResolution";
+            this.cmbxResolution.Size = new System.Drawing.Size(121, 21);
+            this.cmbxResolution.TabIndex = 4;
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.Color.Lime;
+            this.progressBar.Location = new System.Drawing.Point(87, 85);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(618, 17);
+            this.progressBar.TabIndex = 5;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(672, 133);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 6;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click_1);
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(712, 89);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(21, 13);
+            this.lblPercentage.TabIndex = 7;
+            this.lblPercentage.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 175);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.cmbxResolution);
+            this.Controls.Add(this.tbxUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResolution);
+            this.Controls.Add(this.lblUrl);
+            this.Name = "Form1";
+            this.Text = "Download Youtube Video";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Label lblResolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxUrl;
+        private System.Windows.Forms.ComboBox cmbxResolution;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblPercentage;
+    }
+}
+
---
+++ ApothAudioTools/Form1.cs
@@ -0,0 +1,142 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using VideoLibrary;
+using YoutubeExtractor;
+
+namespace ApothAudioTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbxResolution.SelectedIndex = 0;
+        }
+
+        //private void btnDownload_Click(object sender, EventArgs e)
+        private void btnDownload_Click_1(object sender, EventArgs e)
+        {
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+
+            //sender.ProtocolVersion = HttpVersion.Version10; // THIS DOES THE TRICK
+            //ServicePointManager.Expect100Continue = true;
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            //ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
+            //get video url
+            //IEnumerable<VideoInfo> videos = DownloadUrlResolver.GetDownloadUrls(tbxUrl.Text);
+            var videos = DownloadUrlResolver.GetDownloadUrls(tbxUrl.Text, false);
+
+            //MediaDownloader();
+
+            //VideoDownloader(videos);
+
+            AudioDownloader(videos);
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, ProgressEventArgs e)
+        {
+            //update the progress bar
+            Invoke(new MethodInvoker(delegate ()
+            {
+                progressBar.Value = (int)e.ProgressPercentage;
+                lblPercentage.Text = $"{string.Format("{0:0.##}", e.ProgressPercentage)}%";
+                progressBar.Update();
+            }));
+        }
+
+        public void VideoDownloader(IEnumerable<VideoInfo> videos)
+        {
+            
+            //VideoInfo video = videos.First(p => p.VideoType == VideoType.Mp4 && p.Resolution == Convert.ToInt32(cmbxResolution.Text));
+            VideoInfo video = videos.First(p => p.VideoType == VideoType.Mp4 && p.Resolution == 360);
+
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            //download video
+            VideoDownloader downloader = new VideoDownloader(video, Path.Combine(@"C:\Users\david.waidmann\Downloads\", video.Title + video.VideoExtension));
+            downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
+
+            //create a new thread to download the file
+            Thread thread = new Thread(() => { downloader.Execute(); }) { IsBackground = true };
+            thread.Start();
+        }
+
+        public void AudioDownloader(IEnumerable<VideoInfo> videos)
+        {
+            /*
+             * We want the first extractable video with the highest audio quality.
+             */
+            VideoInfo video = videos
+                //.Where(info => info.CanExtractAudio)
+                .OrderByDescending(info => info.AudioBitrate)
+                .First();
+
+            /*
+             * If the video has a decrypted signature, decipher it
+             */
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            /*
+             * Create the audio downloader.
+             * The first argument is the video where the audio should be extracted from.
+             * The second argument is the path to save the audio file.
+             */
+            var audioDownloader = new AudioDownloader(video, Path.Combine(@"C:\Users\david.waidmann\Downloads\", video.Title + video.AudioExtension));
+
+            // Register the progress events. We treat the download progress as 85% of the progress and the extraction progress only as 15% of the progress,
+            // because the download will take much longer than the audio extraction.
+            audioDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage * 0.85);
+            audioDownloader.AudioExtractionProgressChanged += (sender, args) => Console.WriteLine(85 + args.ProgressPercentage * 0.15);
+
+            /*
+             * Execute the audio downloader.
+             * For GUI applications note, that this method runs synchronously.
+             */
+            audioDownloader.Execute();
+        }
+
+        public void MediaDownloader()
+        {
+            var source = @"C:\Users\david.waidmann\Downloads\";
+            var youtube = YouTube.Default;
+            //var vid = youtube.GetVideo("<video url>");
+
+            //sender.ProtocolVersion = HttpVersion.Version10; // THIS DOES THE TRICK
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
+            var vid = youtube.GetVideo(tbxUrl.Text);
+            File.WriteAllBytes(source + vid.FullName, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = source + vid.FullName };
+            var outputFile = new MediaFile { Filename = $"{source + vid.FullName}.mp3" };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+            }
+        }
+    }
+}
